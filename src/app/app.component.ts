74bbf569e051aec764815a07ce5b3db94c49fdbb
Anton Trukhan <Anton.Trukhan@portaone.com>
2020-06-24 16:53:45 +0300

    Version 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,115 +1,116 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
-import {Params$Resource$Events$List, Schema$Event, Schema$Events} from './GoogleCalendarv3Models';
+import { Params$Resource$Events$List, Schema$Event, Schema$Events } from './GoogleCalendarv3Models';
 
-const CALENDARS_URL_MAP = {
-  GREEN_ZONE: 'b2sunei7rseftrnsjf9ab54jd4@group.calendar.google.com',
-  YELLOW_ZONE: '1ok7p3motcv4bif3m9llor0k0c@group.calendar.google.com',
-  RED_ZONE: 'fmtdb38a16cd3b7i6r7bmrkup4@group.calendar.google.com'
+const CALENDARS_URL_MAP: { [key: string]: string } = {
+    GREEN_ZONE: 'b2sunei7rseftrnsjf9ab54jd4@group.calendar.google.com',
+    YELLOW_ZONE: '1ok7p3motcv4bif3m9llor0k0c@group.calendar.google.com',
+    RED_ZONE: 'fmtdb38a16cd3b7i6r7bmrkup4@group.calendar.google.com'
 };
 
-const ZONES_MAP = {
-  GREEN_ZONE: 'GREEN_ZONE',
-  YELLOW_ZONE: 'YELLOW_ZONE',
-  RED_ZONE: 'RED_ZONE'
+const ZONES_MAP: { [key: string]: string } = {
+    GREEN_ZONE: 'GREEN_ZONE',
+    YELLOW_ZONE: 'YELLOW_ZONE',
+    RED_ZONE: 'RED_ZONE'
 };
 
-const FIRST_ELEM_COUNT = 3;
+const FIRST_ELEM_COUNT: number = 3;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    // tslint:disable-next-line:component-selector
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'dev-calendar';
-  dateNow: Date = new Date();
+    title = 'dev-calendar';
+    dateNow: Date = new Date();
 
-  zonesArray = ['GREEN_ZONE', 'YELLOW_ZONE', 'RED_ZONE'];
-  zonesMap = ZONES_MAP;
+    zonesArray: string[] = ['GREEN_ZONE', 'YELLOW_ZONE', 'RED_ZONE'];
+    zonesMap: { [key: string]: string } = ZONES_MAP;
 
-
-  calendarsMap: { [key: string]: Schema$Events } = {
-    GREEN_ZONE: null,
-    YELLOW_ZONE: null,
-    RED_ZONE: null
-  };
-
-  eventsMap: { [key: string]: Schema$Event[] } = {
-    GREEN_ZONE: null,
-    YELLOW_ZONE: null,
-    RED_ZONE: null
-  };
-
-  firstEventsMap: { [key: string]: Schema$Event[] } = {
-    GREEN_ZONE: null,
-    YELLOW_ZONE: null,
-    RED_ZONE: null
-  };
-
-  constructor(private http: HttpClient,
-              private cdr: ChangeDetectorRef) {
-    this.updateStorage(ZONES_MAP.GREEN_ZONE);
-    this.updateStorage(ZONES_MAP.YELLOW_ZONE);
-    this.updateStorage(ZONES_MAP.RED_ZONE);
-  }
-
-  get isReady() {
-    return this.eventsMap.GREEN_ZONE && this.eventsMap.YELLOW_ZONE && this.eventsMap.RED_ZONE;
-  }
-
-  private getData(calendarId: string): Observable<Schema$Events> {
-    const a = new Date();
-    const calcMonth = a.getMonth() - 6;
-    const month = calcMonth < 0 ? 11 - (calcMonth) : calcMonth;
-    const year = calcMonth < 0 ? a.getFullYear() - 1 : a.getFullYear();
-    const params: Params$Resource$Events$List = {
-      key: 'AIzaSyDD3ZPB3yZQWqJ1l9fU0W5FvqXI8G8gCVA',
-      prettyPrint: true,
-      timeMin: `${year}-${month < 10 ? '0' + month : month}-01T10:00:00Z`,
-      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    calendarsMap: { [key: string]: Schema$Events } = {
+        GREEN_ZONE: null,
+        YELLOW_ZONE: null,
+        RED_ZONE: null
     };
-    return this.http.get<Schema$Events>(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`, {params: params as any});
-  }
 
-  private setZoneDate(storage: string, obj: any) {
-    this.calendarsMap[storage] = obj;
-    this.eventsMap[storage] = this.getEventsAtDate(obj);
-    this.firstEventsMap[storage] = this.getFirstEvents(this.getEventsAtDate(obj, this.dateNow, 'b'));
-    this.cdr.markForCheck();
-  }
+    eventsMap: { [key: string]: Schema$Event[] } = {
+        GREEN_ZONE: null,
+        YELLOW_ZONE: null,
+        RED_ZONE: null
+    };
 
-  private updateStorage(storage: string) {
-    this.getData(CALENDARS_URL_MAP[storage]).subscribe(obj => {
-        let storageObj = {};
-        storageObj[storage] = obj;
-        this.setZoneDate(storage, obj);
-      }
-    );
-  }
+    firstEventsMap: { [key: string]: Schema$Event[] } = {
+        GREEN_ZONE: null,
+        YELLOW_ZONE: null,
+        RED_ZONE: null
+    };
 
-  getEventsAtDate(events: Schema$Events, date = new Date(), dateControl: 'm' | 'l' | 'b' = 'm'): Schema$Event[] {
-    return events.items.filter((obj: Schema$Event) =>
-      dateControl === 'm'
-        ? Date.parse(obj.start.date) < date.getTime()
-        && date.getTime() < Date.parse(obj.end.date)
-        : dateControl === 'b'
-        ? date.getTime() < Date.parse(obj.start.date)
-        : date.getTime() > Date.parse(obj.end.date)
-    );
-  }
+    constructor(private http: HttpClient, private cdRef: ChangeDetectorRef) {
+        this.updateStorage(ZONES_MAP.GREEN_ZONE);
+        this.updateStorage(ZONES_MAP.YELLOW_ZONE);
+        this.updateStorage(ZONES_MAP.RED_ZONE);
+    }
 
-  getFirstEvents(array: any[]) {
-    return array.splice(0, FIRST_ELEM_COUNT);
-  }
+    get isReady(): boolean {
+        return !!this.eventsMap.GREEN_ZONE && !!this.eventsMap.YELLOW_ZONE && !!this.eventsMap.RED_ZONE;
+    }
 
-  getLeftDays(date: string) {
-    let cmp = Date.parse(date) - Date.now() - 2 * 3600 * 1000;
-    return (cmp / (1000 * 3600 * 24)) | 0;
-  }
+    private getData(calendarId: string): Observable<Schema$Events> {
+        const params: Params$Resource$Events$List = {
+            key: 'AIzaSyDD3ZPB3yZQWqJ1l9fU0W5FvqXI8G8gCVA',
+            prettyPrint: true,
+            /**
+             * re-check month calculation, problem with january
+             */
+            timeMin: new Date(new Date().setMonth(new Date().getMonth() - 6)).toISOString(),
+            timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+        };
+        return this.http.get<Schema$Events>(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`, {
+            params: params as any
+        });
+    }
 
+    private setZoneDate(storage: string, obj: any): void {
+        this.calendarsMap[storage] = obj;
+        this.eventsMap[storage] = this.getEventsAtDate(obj);
+        this.firstEventsMap[storage] = this.getFirstEvents(this.getEventsAtDate(obj, this.dateNow, 'b'));
+        this.cdRef.markForCheck();
+    }
+
+    private updateStorage(storage: string): void {
+        this.getData(CALENDARS_URL_MAP[storage]).subscribe(obj => {
+            const storageObj: object = {};
+            storageObj[storage] = obj;
+            this.setZoneDate(storage, obj);
+        });
+    }
+
+    getEventsAtDate(
+        events: Schema$Events,
+        date: Date = new Date(),
+        dateControl: 'm' | 'l' | 'b' = 'm'
+    ): Schema$Event[] {
+        return events.items.filter((obj: Schema$Event) =>
+            dateControl === 'm'
+                ? Date.parse(obj.start.date) < date.getTime() && date.getTime() < Date.parse(obj.end.date)
+                : dateControl === 'b'
+                ? date.getTime() < Date.parse(obj.start.date)
+                : date.getTime() > Date.parse(obj.end.date)
+        );
+    }
+
+    getFirstEvents(array: any[]): Schema$Event[] {
+        return array.splice(0, FIRST_ELEM_COUNT);
+    }
+
+    getLeftDays(date: string): number {
+        const cmp: number = Date.parse(date) - Date.now() - 2 * 3600 * 1000;
+        return (cmp / (1000 * 3600 * 24)) | 0;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
-import {MzCollapsibleModule, MzTabModule} from 'ngx-materialize';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { MzCollapsibleModule, MzTabModule } from 'ngx-materialize';
 
-import {MaterialModule} from './material.module';
-
-import {AppComponent} from './app.component';
+import { MaterialModule } from './material.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    MzTabModule,
-    MzCollapsibleModule,
-    BrowserModule,
-    MaterialModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        MzTabModule,
+        MzCollapsibleModule,
+        BrowserModule,
+        MaterialModule,
+        HttpClientModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
