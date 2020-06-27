694f3123c16b827153499cd7a45e81ab3acaa85d
Anton Trukhan <deko71dekobraz@gmail.com>
2020-06-27 08:23:45 +0300

    Adjust styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
+
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MaterialModule, FlexLayoutModule],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MatTabsModule,
+        MatExpansionModule
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
